018e90614a400484f02f3ddf81bfbbddbae8ca20
Kale-Ko <github@kaleko.ga>
2021-08-19 13:54:33 -0400

    Update

--- src/Physics/FirePhysics.js
+++ src/Physics/FirePhysics.js
@@ -5,15 +5,11 @@ import FireTile from "../Tiles/Fire.js"
 export default class FirePhysics {
     update(world, tile, pos) {
         if (tile.type == "fire") {
-            tile.temperature -= 0.05
-
-            console.log(tile.temperature)
+            tile.temperature -= 0.5
 
             if (tile.temperature < 2) world.set(new Vector2(pos.x, pos.y), new AirTile())
-        } else {
-            if (!tile.flammable) return
-
-            tile.temperature -= 0.05
+        } else if (tile.flammable) {
+            tile.temperature -= 0.5
 
             if (tile.temperature >= 6 && world.get(new Vector2(pos.x, pos.y - 1)).name == "air") world.set(new Vector2(pos.x, pos.y - 1), new FireTile())
         }
